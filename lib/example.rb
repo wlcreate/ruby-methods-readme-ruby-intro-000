f059a2905c042f1d1b082ce1984f32d25bda4f70
Waverley Leung <waverley.nyc@gmail.com>
2020-07-07 18:30:50 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,26 @@
-# Use this space to code along with the readme
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+say_hello_world_ten_times
+
+say_hello_world_ten_times
+
+def greeting
+  puts "Hello World"
+end
+
+greeting
 
 phrase = "Hello World!"
 puts phrase
